9e4dbe35e4d5d7312f6188ccff900662790a2a53
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-03-19 16:11:48 +0100

    img uploads shown

--- app/proxy.conf.api.js
+++ app/proxy.conf.api.js
@@ -7,6 +7,15 @@ const PROXY_CONFIG =
       secure: false,
       changeOrigin: true,
       logLevel: 'debug'
+    },
+    '/uploads/*': {
+      target: 'https://enigmatic-cliffs-34872.herokuapp.com/',
+      // target: 'http://localhost:1337',
+      // pathRewrite: { '^/api/': '' },
+      secure: false,
+      changeOrigin: true,
+      logLevel: 'debug'
     }
+
   };
 module.exports = PROXY_CONFIG;
