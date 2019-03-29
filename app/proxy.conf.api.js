eeb8e501f89ddb0acffef57877566fafbbbb2c20
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-03-29 13:27:54 +0100

    changes for connection to mongodb

--- app/proxy.conf.api.js
+++ app/proxy.conf.api.js
@@ -1,16 +1,16 @@
 const PROXY_CONFIG =
   {
     '/api/*': {
-      target: 'https://enigmatic-cliffs-34872.herokuapp.com/',
-      // target: 'http://localhost:1337',
+      // target: 'https://enigmatic-cliffs-34872.herokuapp.com/',
+      target: 'http://localhost:1337',
       pathRewrite: { '^/api/': '' },
       secure: false,
       changeOrigin: true,
       logLevel: 'debug'
     },
     '/uploads/*': {
-      target: 'https://enigmatic-cliffs-34872.herokuapp.com/',
-      // target: 'http://localhost:1337',
+      // target: 'https://enigmatic-cliffs-34872.herokuapp.com/',
+      target: 'http://localhost:1337',
       // pathRewrite: { '^/api/': '' },
       secure: false,
       changeOrigin: true,
