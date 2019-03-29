0a112d1abcf899fdc868eb4be28a44b68edc9c86
Adrian Tudorache <a.p.tudorache@gmail.com>
2019-03-29 15:37:50 +0100

    display markdown and document links

--- app/src/components/Cms/index.js
+++ app/src/components/Cms/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { MarkdownPreview } from 'react-marked-markdown';
 import Strapi from 'strapi-sdk-javascript/build/main';
+
+
+const documentLinks = (documents = []) => (<ul>
+  {documents.map((d) => <li><a href={d.url}>{d.name}</a></li>)}
+</ul>);
 
 const strapi = new Strapi('api');
 class Cms extends React.Component {
@@ -19,15 +25,19 @@ class Cms extends React.Component {
   }
   render() {
     return (
-      <section>
-        {this.state.articles.map((article) => (
-          <article className={'cms'}>
-            <img src={article.Image.url} alt={''} />
-            <h2>{article.Title}</h2>
-            <p>{article.Content}</p>
-          </article>
-      ))}
-      </section>
+      <Fragment>
+        <section>
+          {this.state.articles.map((article) => (
+            <article className={'cms'}>
+              <img src={article.image.url} alt={''} />
+              <h2>{article.title}</h2>
+              <MarkdownPreview value={article.content} />
+              {article['documents-title'] && <h2>{article['documents-title']}</h2>}
+              {documentLinks(article.documents)}
+            </article>
+          ))}
+        </section>
+      </Fragment>
     );
   }
 }
